0c49e9fe0d5035aaa8e2448644d337d2d3b33824
root <root@ip-172-31-14-176.ap-south-1.compute.internal>
2022-05-06 06:24:02 +0000

    sudha first commit

---
+++ git.java
@@ -0,0 +1,11 @@
+git config --global user.name  “girishmaster”
+git config --global user.email “girishgirish311@gmail.com”
+git config --global core.editor vim
+git config --global core.compression 2
+git config --global diff.tool vim.diff
+git config --global core.editor vim2
+git config --global core.compression
+
+
+
+
